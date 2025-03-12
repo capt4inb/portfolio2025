cfa9b6fdf5a5c2ecaf4d7ad17b4ece98c0e01841
Phu Le <trieuphuisme@gmail.com>
2025-03-12 15:12:34 +0700

    add: projects sections

--- index.js
+++ index.js
@@ -168,13 +168,13 @@ function addBotResponse() {
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-primary" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
             </svg>
-            <a href="#" class="text-primary hover:underline">036 5454 067</a>
+            <a href="tel:+84365454067" class="text-primary hover:underline">036 5454 067</a>
           </div>
           <div class="flex items-center gap-2">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-primary" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
             </svg>
-            <a href="#" class="text-primary hover:underline">trieuphuisme@gmail.com</a>
+            <a href="mailto:trieuphuisme@gmail.com" class="text-primary hover:underline">trieuphuisme@gmail.com</a>
           </div>
           <div class="flex items-center gap-2">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-primary" fill="none" viewBox="0 0 24 24" stroke="currentColor">
